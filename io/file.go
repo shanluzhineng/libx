ae4b1cafe99471c0c01786d625c69b11d5a06ef8
qing zicheng <jeseeqing@gmail.com>
2023-04-06 21:40:33 +0800

    add some io func

---
+++ io/file.go
@@ -0,0 +1,23 @@
+package io
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func Exists(path string) bool {
+	//os.Stat获取文件信息
+	_, err := os.Stat(path)
+	if err != nil {
+		return os.IsExist(err)
+	}
+	return true
+}
+
+// EnsurePath is used to make sure a path exists
+func EnsurePath(path string, dir bool) error {
+	if !dir {
+		path = filepath.Dir(path)
+	}
+	return os.MkdirAll(path, 0755)
+}
