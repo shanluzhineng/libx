6a064a26d3702e04a149c20c9c63c6fe77fd3b9b
zcqing <zcqing@wokanbit.com>
2023-06-11 15:39:57 +0800

    add ValueToPtr in lang package

--- lang/converter.go
+++ lang/converter.go
@@ -1,5 +1,12 @@
 package lang
 
+import "time"
+
+type KnowedStructType interface {
+	int8 | int16 | int32 | int64 | float32 | float64 | uint8 | uint16 | uint32 | uint64 |
+		bool | time.Time
+}
+
 // 将bool值转换为指针
 func BoolToPtr(v bool) *bool {
 	return &v
@@ -24,4 +31,9 @@ func PtrToInt(v *int) int {
 		return 0
 	}
 	return *v
+}
+
+// 获取值的指针
+func ValueToPtr[T any](v T) *T {
+	return &v
 }
