a8e4b1598dba08a49d4dc3eb51fd7f08387f466e
qing zicheng <jeseeqing@gmail.com>
2023-06-16 13:17:13 +0800

    add Read and Write file func in io packages

--- io/file.go
+++ io/file.go
@@ -1,6 +1,8 @@
 package io
 
 import (
+	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 )
@@ -20,4 +22,49 @@ func EnsurePath(path string, dir bool) error {
 		path = filepath.Dir(path)
 	}
 	return os.MkdirAll(path, 0755)
+}
+
+// 读取文件
+func ReadFile(filePath string) ([]byte, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer file.Close()
+
+	byteValue, err := io.ReadAll(file)
+	return byteValue, err
+}
+
+// 将内容存入到文件中，自动换行
+func WriteToFile(filePath string, data string) error {
+	f, err := os.OpenFile(filePath,
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	if _, err := f.WriteString(data + "\n"); err != nil {
+		err = fmt.Errorf("写入数据到文件%s时出错,错误信息:%s", filePath, err.Error())
+		return err
+	}
+	return nil
+}
+
+// 将内容存入到文件中，自动换行
+func WriteLinesToFile(filePath string, lines []string) error {
+	f, err := os.OpenFile(filePath,
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	for _, eachLine := range lines {
+		if _, err := f.WriteString(eachLine); err != nil {
+			err = fmt.Errorf("写入数据到文件%s时出错,错误信息:%s", filePath, err.Error())
+			return err
+		}
+	}
+	return nil
 }
