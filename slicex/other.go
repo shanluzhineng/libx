7917c02442c24fdbc10a5c3e0223af34bef834be
zcqing <jeseeqing@gmail.com>
2024-03-20 17:39:46 +0800

    add PartitionSlice func in slicex package

--- slicex/other.go
+++ slicex/other.go
@@ -1,6 +1,6 @@
 package slicex
 
-// chunk a slice to
+// 将一个切片分割成指定数量的切片,每个子切片的长度都是chunkSize
 func ChunkSlice[SV any](input []SV, chunkSize int) [][]SV {
 	var result [][]SV
 
@@ -15,4 +15,28 @@ func ChunkSlice[SV any](input []SV, chunkSize int) [][]SV {
 	}
 
 	return result
+}
+
+// 将一个切片均匀地分配到多个子切片中，子切片的数量由参数 partitionCount 决定
+// 如果原始切片的长度小于 partitionCount，则每个子切片只包含一个元素
+func PartitionSlice[SV any](input []SV, partitionCount int) [][]SV {
+	var partitionSlice [][]SV
+	count := 0
+	if len(input) < partitionCount {
+		count = len(input)
+	} else {
+		count = partitionCount
+	}
+	partitionSlice = make([][]SV, count)
+
+	for i := 0; i < count; i++ {
+		partitionSlice[i] = make([]SV, 0)
+	}
+
+	for i, v := range input {
+		index := i % count
+		partitionSlice[index] = append(partitionSlice[index], v)
+	}
+
+	return partitionSlice
 }
